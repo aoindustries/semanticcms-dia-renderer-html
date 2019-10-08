20532d7d2fe38b21c9dce49a6ae34f7c79fc2c5d
AO Industries, Inc. <support@aoindustries.com>
2019-10-08 02:47:06 -0500

    Changed CountConcurrencyFilter from a <filter> to a <listener> named
    CountConcurrencyListener.

--- src/main/java/com/semanticcms/dia/renderer/html/DiaHtmlRenderer.java
+++ src/main/java/com/semanticcms/dia/renderer/html/DiaHtmlRenderer.java
@@ -34,7 +34,7 @@ import com.aoindustries.util.Sequence;
 import com.aoindustries.util.UnsynchronizedSequence;
 import com.aoindustries.util.WrappedException;
 import com.aoindustries.util.concurrent.ConcurrencyLimiter;
-import com.semanticcms.core.controller.CountConcurrencyFilter;
+import com.semanticcms.core.controller.CountConcurrencyListener;
 import com.semanticcms.core.controller.ResourceRefResolver;
 import com.semanticcms.core.controller.SemanticCMS;
 import com.semanticcms.core.model.BookRef;
@@ -408,7 +408,7 @@ final public class DiaHtmlRenderer {
 							);
 						}
 						try {
-							exports = CountConcurrencyFilter.getRecommendedExecutor(servletContext, request).callAll(tasks);
+							exports = CountConcurrencyListener.getRecommendedExecutor(servletContext, request).callAll(tasks);
 						} catch(ExecutionException e) {
 							Throwable cause = e.getCause();
 							if(cause instanceof RuntimeException) throw ((RuntimeException)cause);
